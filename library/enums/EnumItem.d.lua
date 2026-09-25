0cc2a582ffa144f93ed45ec26961cd65f8d715fa
haminpants <samueltfong03@gmail.com>
2026-09-25 04:08:52 -0700

    clarify EnumItem documentation

--- library/enums/EnumItem.d.lua
+++ library/enums/EnumItem.d.lua
@@ -6,83 +6,83 @@
 ---@field FullName string # [Read] The fully qualified name of the enum (e.g., Enum.EaseType.InQuad).
 ---@field EnumType string # [Read] The name of the enum type (e.g., EaseType for Enum.EaseType.InQuad).
 
----See [Enum.ControllerKeyCode](https://haminpants.github.io/mililua/class/Enum.ControllerKeyCode.html) for all enum values.
+---Represents an enum value from [Enum.ControllerKeyCode](https://haminpants.github.io/mililua/class/Enum.ControllerKeyCode.html).
 ---@class EnumItem.ControllerKeyCode : EnumItem
 
----See [Enum.ControllerNavigationDir](https://haminpants.github.io/mililua/class/Enum.ControllerNavigationDir.html) for all enum values.
+---Represents an enum value from [Enum.ControllerNavigationDir](https://haminpants.github.io/mililua/class/Enum.ControllerNavigationDir.html).
 ---@class EnumItem.ControllerNavigationDir : EnumItem
 
----See [Enum.ControllerNavigationEventType](https://haminpants.github.io/mililua/class/Enum.ControllerNavigationEventType.html) for all enum values.
+---Represents an enum value from [Enum.ControllerNavigationEventType](https://haminpants.github.io/mililua/class/Enum.ControllerNavigationEventType.html).
 ---@class EnumItem.ControllerNavigationEventType : EnumItem
 
----See [Enum.ControllerNavigationMode](https://haminpants.github.io/mililua/class/Enum.ControllerNavigationMode.html) for all enum values.
+---Represents an enum value from [Enum.ControllerNavigationMode](https://haminpants.github.io/mililua/class/Enum.ControllerNavigationMode.html).
 ---@class EnumItem.ControllerNavigationMode : EnumItem
 
----See [Enum.CursorEventType](https://haminpants.github.io/mililua/class/Enum.CursorEventType.html) for all enum values.
+---Represents an enum value from [Enum.CursorEventType](https://haminpants.github.io/mililua/class/Enum.CursorEventType.html).
 ---@class EnumItem.CursorEventType : EnumItem
 
----See [Enum.CustomVariableEntityType](https://haminpants.github.io/mililua/class/Enum.CustomVariableEntityType.html) for all enum values.
+---Represents an enum value from [Enum.CustomVariableEntityType](https://haminpants.github.io/mililua/class/Enum.CustomVariableEntityType.html).
 ---@class EnumItem.CustomVariableEntityType : EnumItem
 
----See [Enum.Device](https://haminpants.github.io/mililua/class/Enum.Device.html) for all enum values.
+---Represents an enum value from [Enum.Device](https://haminpants.github.io/mililua/class/Enum.Device.html).
 ---@class EnumItem.Device : EnumItem
 
----See [Enum.EaseType](https://haminpants.github.io/mililua/class/Enum.EaseType.html) for all enum values.
+---Represents an enum value from [Enum.EaseType](https://haminpants.github.io/mililua/class/Enum.EaseType.html).
 ---@class EnumItem.EaseType : EnumItem
 
----See [Enum.ImageFillType](https://haminpants.github.io/mililua/class/Enum.ImageFillType.html) for all enum values.
+---Represents an enum value from [Enum.ImageFillType](https://haminpants.github.io/mililua/class/Enum.ImageFillType.html).
 ---@class EnumItem.ImageFillType : EnumItem
 
----See [Enum.ImageFillHorizontalType](https://haminpants.github.io/mililua/class/Enum.ImageFillHorizontalType.html) for all enum values.
+---Represents an enum value from [Enum.ImageFillHorizontalType](https://haminpants.github.io/mililua/class/Enum.ImageFillHorizontalType.html).
 ---@class EnumItem.ImageFillHorizontalType : EnumItem
 
----See [Enum.ImageFillRadial90Type](https://haminpants.github.io/mililua/class/Enum.ImageFillRadial90Type.html) for all enum values.
+---Represents an enum value from [Enum.ImageFillRadial90Type](https://haminpants.github.io/mililua/class/Enum.ImageFillRadial90Type.html).
 ---@class EnumItem.ImageFillRadial90Type : EnumItem
 
----See [Enum.ImageFillRadialType](https://haminpants.github.io/mililua/class/Enum.ImageFillRadialType.html) for all enum values.
+---Represents an enum value from [Enum.ImageFillRadialType](https://haminpants.github.io/mililua/class/Enum.ImageFillRadialType.html).
 ---@class EnumItem.ImageFillRadialType : EnumItem
 
----See [Enum.ImageFillVerticalType](https://haminpants.github.io/mililua/class/Enum.ImageFillVerticalType.html) for all enum values.
+---Represents an enum value from [Enum.ImageFillVerticalType](https://haminpants.github.io/mililua/class/Enum.ImageFillVerticalType.html).
 ---@class EnumItem.ImageFillVerticalType : EnumItem
 
----See [Enum.ImageMaskSoftEdgeMode](https://haminpants.github.io/mililua/class/Enum.ImageMaskSoftEdgeMode.html) for all enum values.
+---Represents an enum value from [Enum.ImageMaskSoftEdgeMode](https://haminpants.github.io/mililua/class/Enum.ImageMaskSoftEdgeMode.html).
 ---@class EnumItem.ImageMaskSoftEdgeMode : EnumItem
 
----See [Enum.ImageSource](https://haminpants.github.io/mililua/class/Enum.ImageSource.html) for all enum values.
+---Represents an enum value from [Enum.ImageSource](https://haminpants.github.io/mililua/class/Enum.ImageSource.html).
 ---@class EnumItem.ImageSource : EnumItem
 
----See [Enum.ImageType](https://haminpants.github.io/mililua/class/Enum.ImageType.html) for all enum values.
+---Represents an enum value from [Enum.ImageType](https://haminpants.github.io/mililua/class/Enum.ImageType.html).
 ---@class EnumItem.ImageType : EnumItem
 
----See [Enum.KeyEventType](https://haminpants.github.io/mililua/class/Enum.KeyEventType.html) for all enum values.
+---Represents an enum value from [Enum.KeyEventType](https://haminpants.github.io/mililua/class/Enum.KeyEventType.html).
 ---@class EnumItem.KeyEventType : EnumItem
 
----See [Enum.KeyboardKeyCode](https://haminpants.github.io/mililua/class/Enum.KeyboardKeyCode.html) for all enum values.
+---Represents an enum value from [Enum.KeyboardKeyCode](https://haminpants.github.io/mililua/class/Enum.KeyboardKeyCode.html).
 ---@class EnumItem.KeyboardKeyCode : EnumItem
 
----See [Enum.LanguageType](https://haminpants.github.io/mililua/class/Enum.LanguageType.html) for all enum values.
+---Represents an enum value from [Enum.LanguageType](https://haminpants.github.io/mililua/class/Enum.LanguageType.html).
 ---@class EnumItem.LanguageType : EnumItem
 
----See [Enum.ParamType](https://haminpants.github.io/mililua/class/Enum.ParamType.html) for all enum values.
+---Represents an enum value from [Enum.ParamType](https://haminpants.github.io/mililua/class/Enum.ParamType.html).
 ---@class EnumItem.ParamType : EnumItem
 
----See [Enum.ScrollAlignType](https://haminpants.github.io/mililua/class/Enum.ScrollAlignType.html) for all enum values.
+---Represents an enum value from [Enum.ScrollAlignType](https://haminpants.github.io/mililua/class/Enum.ScrollAlignType.html).
 ---@class EnumItem.ScrollAlignType : EnumItem
 
----See [Enum.ScrollDirection](https://haminpants.github.io/mililua/class/Enum.ScrollDirection.html) for all enum values.
+---Represents an enum value from [Enum.ScrollDirection](https://haminpants.github.io/mililua/class/Enum.ScrollDirection.html).
 ---@class EnumItem.ScrollDirection : EnumItem
 
----See [Enum.ScrollLayoutConstraint](https://haminpants.github.io/mililua/class/Enum.ScrollLayoutConstraint.html) for all enum values.
+---Represents an enum value from [Enum.ScrollLayoutConstraint](https://haminpants.github.io/mililua/class/Enum.ScrollLayoutConstraint.html).
 ---@class EnumItem.ScrollLayoutConstraint : EnumItem
 
----See [Enum.StageMode](https://haminpants.github.io/mililua/class/Enum.StageMode.html) for all enum values.
+---Represents an enum value from [Enum.StageMode](https://haminpants.github.io/mililua/class/Enum.StageMode.html).
 ---@class EnumItem.StageMode : EnumItem
 
----See [Enum.TextHorizontalAlignment](https://haminpants.github.io/mililua/class/Enum.TextHorizontalAlignment.html) for all enum values.
+---Represents an enum value from [Enum.TextHorizontalAlignment](https://haminpants.github.io/mililua/class/Enum.TextHorizontalAlignment.html).
 ---@class EnumItem.TextHorizontalAlignment : EnumItem
 
----See [Enum.TextVerticalAlignment](https://haminpants.github.io/mililua/class/Enum.TextVerticalAlignment.html) for all enum values.
+---Represents an enum value from [Enum.TextVerticalAlignment](https://haminpants.github.io/mililua/class/Enum.TextVerticalAlignment.html).
 ---@class EnumItem.TextVerticalAlignment : EnumItem
 
----See [Enum.UIAnimationLayer](https://haminpants.github.io/mililua/class/Enum.UIAnimationLayer.html) for all enum values.
+---Represents an enum value from [Enum.UIAnimationLayer](https://haminpants.github.io/mililua/class/Enum.UIAnimationLayer.html).
 ---@class EnumItem.UIAnimationLayer : EnumItem
