6b3d203ec051e13099f5e4abfd00de8e2b6136b5
haminpants <samueltfong03@gmail.com>
2026-09-25 19:24:02 -0700

    format behaviors as bulleted list

--- library/TweenSequence.d.lua
+++ library/TweenSequence.d.lua
@@ -15,52 +15,45 @@
 local TweenSequence = {}
 
 ---Appends a Tween to the end of the sequence and returns the current instance.
----
----If the added Tween is not in the Initial state, the Tween will not play its animation or call callbacks during the sequence.
----
----Added Tweens should be in the Initial state and are considered to be in the Locked state after being added.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Tweens can only be appended if the Tween Sequence is in the Initial state.
+---- If the added Tween is not in the Initial state, the Tween will not play its animation or call callbacks during the sequence.
+---- Added Tweens should be in the Initial state and are considered to be in the Locked state after being added.
 ---@param tween Tween # The Tween to append to the sequence.
 ---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:Append(tween) end
 
 ---Appends a callback to the end of the sequence and returns the current instance.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Callbacks can only be appended if the Tween Sequence is in the Initial state.
 ---@param callback fun() # The function to call.
 ---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:AppendCallback(callback) end
 
----Appends a timed delay (in seconds) to the end of the sequence and returns the current instance.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---Appends a delay (in seconds) to the end of the sequence and returns the current instance.
+---- Delays can only be appended if the Tween Sequence is in the Initial state.
 ---@param duration number # The wait duration in seconds.
 ---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:AppendInterval(duration) end
 
----Immediately completes Tween Sequence playback, moving the Tween Sequence to the Completed state, and calls the OnStepComplete and OnComplete callbacks of the Tween Sequence and all Tweens.
+---Immediately completes Tween Sequence playback, moving the Tween Sequence to the Completed state.
+---- The OnStepComplete and OnComplete callbacks of the Tween Sequence and all Tweens will be triggered.
 function TweenSequence:Complete() end
 
 ---Inserts a Tween at a specific time and returns the current instance.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Tweens can only be inserted if the Tween Sequence is in the Initial state.
 ---@param time number # The time at which to insert the Tween.
 ---@param tween Tween # The Tween to insert.
 ---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:Insert(time, tween) end
 
 ---Inserts a callback at a specific time and returns the current instance.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Callbacks can only be inserted if the Tween Sequence is in the Initial state.
 ---@param time number # The time at which to insert the callback.
 ---@param callback fun() # The function to call.
----@return TweenSequence # The current Tween Sequence instance.
+---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:InsertCallback(time, callback) end
 
 ---Inserts a Tween at the same time as the current tailing Tween in the sequence and returns the current instance.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Tweens can only be joined if the Tween Sequence is in the Initial state.
 ---@param tween Tween # The Tween to insert.
 ---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:Join(tween) end
@@ -70,53 +63,42 @@ function TweenSequence:Join(tween) end
 function TweenSequence:Kill(complete) end
 
 ---Pauses sequence playback, moving the Tween Sequence to the Paused state.
----
----Regular Tween behaviors apply according to the currently paused Tweens.
----
----Only takes effect if the Tween Sequence is in the Playing state.
+---- Only takes effect if the Tween Sequence is in the Playing state.
+---- Regular Tween behaviors apply according to the currently paused Tweens.
 function TweenSequence:Pause() end
 
 ---Starts sequence playback, moving the Tween Sequence to the Playing state.
----
----Regular Tween behaviors apply according to the currently playing Tweens.
----
----Only takes effect if the Tween Sequence is in the Initial state.
----@return TweenSequence
+---- Only takes effect if the Tween Sequence is in the Initial state.
+---- Regular Tween behaviors apply according to the currently playing Tweens.
+---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:Play() end
 
----Restarts Tween Sequence playback, moving the Tween Sequence to the Playing state and reverting the target values of all Tweens to values stored in their Initial state.
----
----Regular Tween behaviors apply to all affected Tweens.
----
----Only takes effect if the Tween Sequence is not in the Initial state.
+---Restarts Tween Sequence playback, moving the Tween Sequence to the Playing state.
+---- Only takes effect if the Tween Sequence is not in the Initial state.
+---- The target values of all Tweens will be restored to their Initial state.
+---- Regular Tween behaviors apply to all affected Tweens.
 function TweenSequence:Restart() end
 
 ---Resumes sequence playback, moving the Tween Sequence to the Playing state.
----
----Regular Tween behaviors apply according to the resumed Tweens.
----
----Only takes effect if the Tween Sequence is in the Paused state.
+---- Only takes effect if the Tween Sequence is in the Paused state.
+---- Regular Tween behaviors apply according to the resumed Tweens.
 function TweenSequence:Resume() end
 
 ---Sets the number of times the Tween Sequence will complete playback before moving to the Completed state and returns the current instance.
 ---@param loops number # The number of times to loop playback. The Tween Sequence will still play once if set to 0.
----@return TweenSequence # The current Tween Sequence instance.
+---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:SetLoops(loops) end
 
 ---Sets the OnComplete callback and returns the current instance.
----
----The OnComplete callback is called when all the events in the Tween Sequence have ended.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Callbacks can only be set if the Tween Sequence is in the Initial state.
+---- The OnComplete callback is called when all the events in the Tween Sequence have ended.
 ---@param onComplete fun() # The function to call when the Tween Sequence completes.
----@return TweenSequence # The current Tween Sequence instance.
+---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:SetOnComplete(onComplete) end
 
 ---Sets the OnStepComplete callback and returns the current instance.
----
----The OnStepComplete callback is called when the Tween Sequence loops or completes playback.
----
----Only takes effect if the Tween Sequence is in the Initial state.
+---- Callbacks can only be set if the Tween Sequence is in the Initial state.
+---- The OnStepComplete callback is called when the Tween Sequence loops or completes playback.
 ---@param onStepComplete fun() # The function to call when the Tween Sequence loops or completes playback.
----@return TweenSequence # The current Tween Sequence instance.
+---@return TweenSequence sequence # The current Tween Sequence instance.
 function TweenSequence:SetOnStepComplete(onStepComplete) end
